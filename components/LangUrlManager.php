dfe8027489e93ac8278f3e2706fe1f3f532e590e
York <yorkshp@gmail.com>
2017-10-16 16:22:08 +0800

    upload

---
+++ components/LangUrlManager.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phpCreator
+ * Date: 19.08.2017
+ * Time: 21:42
+ */
+
+namespace frontend\components;
+
+use common\models\Language;
+use yii\web\UrlManager;
+class LangUrlManager extends UrlManager{
+
+    public function createUrl($params){
+
+        if(isset($params['lang_id'])){
+
+
+            $lang = Language::findOne($params['lang_id']);
+            if($lang === null){
+                $lang = Language::getDefaultLang();
+            }
+            unset($params);
+        }
+        else{
+            $lang = Language::getCurrent();
+        }
+
+        $url = parent::createUrl($params);
+
+        if($url == '/'){
+            return '/'.$lang->code;
+        }
+        else{
+            return '/'.$lang->code.$url;
+        }
+    }
+
+
+}
